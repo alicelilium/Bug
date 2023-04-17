6d3fdc6115d344b07f0bfc4380c614410edcf748
alice <107301994+alicelilium@users.noreply.github.com>
2023-04-16 17:25:53 -0700

    Add files via upload

---
+++ Bug/BugGame.java
@@ -0,0 +1,69 @@
+/*  Alice Lily
+    Allows the user to control a Bug object created from the Bug class
+    They can move the bug one at a time, turn it around, and display its
+    position on the screen. The user can exit at any time.              */
+
+
+//Import for Scanner
+import java.util.Scanner;
+import bugpkg.Bug;
+
+public class BugGame {
+
+    public static void main(String[] args) {
+        //Initialize new bug for game, scanner for input and boolean to keep loop active
+        Bug bug = new Bug();
+        Scanner scan = new Scanner(System.in);
+        boolean play = true;
+
+        //List off the options and display the starting status of the bug
+        System.out.println("Welcome to the bug moving game! Here are your commands: ");
+        System.out.println("1 - Move the bug in it's current direction.");
+        System.out.println("2 - Turn the bug to the other direction.");
+        System.out.println("3 - Output the current status of the bug.");
+        System.out.println("4 - Start over in a new position. ");
+        System.out.println("Any other number - Exit the game. \n");
+        System.out.println(bug.toString());
+
+        while (play) {
+
+            //Prompt for command
+            System.out.println("Please enter a command (enter 5 to show the commands again): ");
+            int command = scan.nextInt();
+
+
+            switch (command) {
+                case 1:         //Move bug when 1 is entered
+                    bug.move();
+                    break;
+                case 2:         //Turn bug when 2 is entered
+                    bug.turn();
+                    break;
+                case 3:         //Display status of bug when 3 is entered
+                    System.out.println(bug.toString());
+                    break;
+                case 4:         //Initialize new bug when 4 is entered
+                    bug = new Bug();
+                    System.out.println("Restarting... \n" + bug.toString());
+                    break;
+                case 5:         //Re-display commands when 5 is entered
+                    System.out.println("1 - Move the bug in it's current direction.");
+                    System.out.println("2 - Turn the bug to the other direction.");
+                    System.out.println("3 - Output the current status of the bug.");
+                    System.out.println("4 - Start over in a new position. ");
+                    System.out.println("Any other number - Exit the game. \n");
+                    break;
+                default:        //If any other number entered, prompt to confirm end of game.
+                    System.out.println("Are you sure you'd like to quit? 0 to confirm," +
+                                            " anything else to keep playing:");
+                    if (scan.nextInt() == 0) {
+                        System.out.println("Thank you for playing!");
+                        play = false;
+                        break;
+                    } else
+                        System.out.println("Returning to the game!");
+                        break;
+            }
+        }
+    }
+}
---
+++ Bug/bugpkg/Bug.java
@@ -0,0 +1,89 @@
+/*  Alice Lily
+    Creates the Bug class - an interactive "bug" that can move along
+    a wire to the left or the right (can be turned). Establishes all
+    the appropriate methods that can affect the bug or be used to
+    view its status.		*/
+	
+package bugpkg;
+
+public class Bug {
+
+    //Establish the bounds of the wire
+    private final int MAX_LEFT = -50;
+    private final int MAX_RIGHT = 50;
+
+    //Variables for position and direction used within the class
+    private int position;
+    private int direction = 1;
+
+    //Places the bug at a random point along the wire
+    public Bug() {
+        position = (int)Math.floor(Math.random() * (Math.abs(MAX_LEFT)+MAX_RIGHT)- 50);
+    }
+
+    //User can construct with an initial point on wire
+    public Bug(int startpoint) {
+        position = startpoint;
+
+    }
+
+    //Internal check for if bug has met the bounds
+    private boolean canMove() {
+        if (position == MAX_RIGHT && direction == 1)
+            return false;
+        else if (position == MAX_LEFT && direction == 0)
+            return false;
+        else
+            return true;
+    }
+
+    //Return direction of the bug
+    public String getDirection() {
+        String direct;
+        if (direction == 1)
+            direct = "right";
+        else
+            direct = "left";
+        return direct;
+    }
+
+    //Return position on wire
+    public int getPosition() {
+        return position;
+    }
+
+    //Allow user to change position if need be within the max range
+    public void setPosition(int newPosition) {
+        if (newPosition >= MAX_LEFT && newPosition <= MAX_RIGHT)
+            position = newPosition;
+    }
+
+    //Move in current direction
+    public void move() {
+        if (canMove()) {
+            if (direction == 1)
+                position++;
+            else
+                position--;
+        }
+    }
+
+    //Turn to other direction
+    public void turn() {
+        if (direction == 1)
+            direction = 0;
+        else
+            direction = 1;
+    }
+
+    //Print position and direction to user
+    public String toString() {
+        if (getPosition() == MAX_LEFT || getPosition() == MAX_RIGHT) {
+            String result = "The bug is at position " + position + " (MAX) facing " + getDirection() + " on the wire.";
+            return result;
+        } else {
+            String result = "The bug is at position " + position + " facing " + getDirection() + " on the wire.";
+            return result;
+        }
+    }
+}
